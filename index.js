469895159c3f0f6359f5c167d67a8f3ba9d24cbc
Arun Narasani <narasani@versa-networks.com>
2018-09-24 20:30:47 -0700

    updates

--- index.js
+++ index.js
@@ -27,135 +27,99 @@
 //   .get('/', (req, res) => res.render('pages/index'))
 //   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
+//==============================
+
 // See https://github.com/dialogflow/dialogflow-fulfillment-nodejs
 // for Dialogflow fulfillment library docs, samples, and to report issues
-'use strict';
+// 'use strict';
  
-// const functions = require('firebase-functions');
-const {WebhookClient} = require('dialogflow-fulfillment');
-const {Card, Suggestion} = require('dialogflow-fulfillment'); 
-const express = require('express');
-const path = require('path');
-const port = process.env.PORT || 5000;
+// // const functions = require('firebase-functions');
+// const {WebhookClient} = require('dialogflow-fulfillment');
+// const {Card, Suggestion} = require('dialogflow-fulfillment'); 
+// const express = require('express');
+// const path = require('path');
+// const port = process.env.PORT || 5000;
 
-var app = express();
+// var app = express();
 
-process.env.DEBUG = 'dialogflow:debug'; // enables lib debugging statements
+// process.env.DEBUG = 'dialogflow:debug'; // enables lib debugging statements
  
-app.get('/', function (req, res) {
-  res.send('Welcome to qos-bot API host!');
-})
+// app.get('/', function (req, res) {
+//   res.send('Welcome to qos-bot API host!');
+// })
 
-app.post('/dialogflowFirebaseFulfillment', function (req, res) {
+// app.post('/dialogflowFirebaseFulfillment', function (req, res) {
 
-	console.log('request = ' + req);
-	const agent = new WebhookClient({
-	  request: req,
-	  response: res
-	});
-	// console.log('Dialogflow Request headers: ' + JSON.stringify(req.headers));
-	// console.log('Dialogflow Request body: ' + JSON.stringify(req.body));
+// 	console.log('request = ' + req);
+// 	const agent = new WebhookClient({
+// 	  request: req,
+// 	  response: res
+// 	});
+// 	// console.log('Dialogflow Request headers: ' + JSON.stringify(req.headers));
+// 	// console.log('Dialogflow Request body: ' + JSON.stringify(req.body));
 
-	function livestatus(agent) {
-	  const device = agent.parameters.device;
-	  const command = agent.parameters.command;
-	  agent.add(`${command} for ${device} is ` + Math.random());
-	}
-
-    function trivia(agent){
-      console.log("1");
-    	var trivia_req = require('sync-request');
-      console.log("2");
-    	var trivia_res = trivia_req('GET', 'http://numbersapi.com/random/trivia');
-      console.log("3");
-    	agent.add(trivia_res.body.toString('utf-8'));
-    }
-
-	let intentMap = new Map();
-	intentMap.set('live status', livestatus);
-	intentMap.set('trivia', trivia);
-	agent.handleRequest(intentMap);
-})
-
-app.listen(port, () => console.log(`qos-bot-api app listening on port ${port}!`))
-
-// exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, response) => {
-//   const agent = new WebhookClient({ request, response });
-//   console.log('Dialogflow Request headers: ' + JSON.stringify(request.headers));
-//   console.log('Dialogflow Request body: ' + JSON.stringify(request.body));
- 
-// //   function welcome(agent) {
-// //     agent.add(`Welcome to my agent!`);
-// //   }
- 
-// //   function fallback(agent) {
-// //     agent.add(`I didn't understand`);
-// //     agent.add(`I'm sorry, can you try again?`);
-// //   }
-
-//   function livestatus(agent) {
-//       const device = agent.parameters.device;
-//       const command = agent.parameters.command;
-//       agent.add(`${command} for ${device} is ` + Math.random());
-//   }
-  
-// //   function trivia(agent) {
-// //         const Http = new XMLHttpRequest();
-// //         const url='http://numbersapi.com/random/trivia';
-// //         Http.open("GET", url);
-// //         Http.send();
-// //         Http.onreadystatechange=(e)=>{
-// //             agent.add(Http.responseText)
-// //         }
-        
-        
-// //   }
+// 	function livestatus(agent) {
+// 	  const device = agent.parameters.device;
+// 	  const command = agent.parameters.command;
+// 	  agent.add(`${command} for ${device} is ` + Math.random());
+// 	}
 
 //     function trivia(agent){
 //       console.log("1");
-//     	var request = require('sync-request');
+//     	var trivia_req = require('sync-request');
 //       console.log("2");
-//     	var out = request('GET', 'http://numbersapi.com/random/trivia');
+//     	var trivia_res = trivia_req('GET', 'http://numbersapi.com/random/trivia');
 //       console.log("3");
-//     	agent.add(out.body.toString('utf-8'));
+//     	agent.add(trivia_res.body.toString('utf-8'));
 //     }
 
-//   // // Uncomment and edit to make your own intent handler
-//   // // uncomment `intentMap.set('your intent name here', yourFunctionHandler);`
-//   // // below to get this function to be run when a Dialogflow intent is matched
-//   // function yourFunctionHandler(agent) {
-//   //   agent.add(`This message is from Dialogflow's Cloud Functions for Firebase editor!`);
-//   //   agent.add(new Card({
-//   //       title: `Title: this is a card title`,
-//   //       imageUrl: 'https://developers.google.com/actions/images/badges/XPM_BADGING_GoogleAssistant_VER.png',
-//   //       text: `This is the body text of a card.  You can even use line\n  breaks and emoji! 💁`,
-//   //       buttonText: 'This is a button',
-//   //       buttonUrl: 'https://assistant.google.com/'
-//   //     })
-//   //   );
-//   //   agent.add(new Suggestion(`Quick Reply`));
-//   //   agent.add(new Suggestion(`Suggestion`));
-//   //   agent.setContext({ name: 'weather', lifespan: 2, parameters: { city: 'Rome' }});
-//   // }
+// 	let intentMap = new Map();
+// 	intentMap.set('live status', livestatus);
+// 	intentMap.set('trivia', trivia);
+// 	agent.handleRequest(intentMap);
+// })
 
-//   // // Uncomment and edit to make your own Google Assistant intent handler
-//   // // uncomment `intentMap.set('your intent name here', googleAssistantHandler);`
-//   // // below to get this function to be run when a Dialogflow intent is matched
-//   // function googleAssistantHandler(agent) {
-//   //   let conv = agent.conv(); // Get Actions on Google library conv instance
-//   //   conv.ask('Hello from the Actions on Google client library!') // Use Actions on Google library
-//   //   agent.add(conv); // Add Actions on Google library responses to your agent's response
-//   // }
-//   // // See https://github.com/dialogflow/dialogflow-fulfillment-nodejs/tree/master/samples/actions-on-google
-//   // // for a complete Dialogflow fulfillment library Actions on Google client library v2 integration sample
+//=========================================
 
-//   // Run the proper function handler based on the matched Dialogflow intent name
-//   let intentMap = new Map();
-// //   intentMap.set('Default Welcome Intent', welcome);
-// //   intentMap.set('Default Fallback Intent', fallback);
-//   intentMap.set('live status', livestatus);
-//   intentMap.set('trivia', trivia);
-//   // intentMap.set('your intent name here', yourFunctionHandler);
-//   // intentMap.set('your intent name here', googleAssistantHandler);
-//   agent.handleRequest(intentMap);
-// });
+'use strict';
+
+const {WebhookClient} = require('dialogflow-fulfillment');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+
+function welcome (agent) {
+    agent.add(`Welcome to Express.JS webhook!`);
+}
+
+function fallback (agent) {
+    agent.add(`I didn't understand`);
+    agent.add(`I'm sorry, can you try again?`);
+}
+
+function WebhookProcessing(req, res) {
+    const agent = new WebhookClient({request: req, response: res});
+    console.info(`agent set`);
+
+    let intentMap = new Map();
+    intentMap.set('trivia', welcome);
+    // intentMap.set('Default Fallback Intent', fallback);
+// intentMap.set('<INTENT_NAME_HERE>', yourFunctionHandler);
+    agent.handleRequest(intentMap);
+}
+
+
+// Webhook
+app.post('/dialogflowFirebaseFulfillment', function (req, res) {
+    console.info(`\n\n>>>>>>> S E R V E R   H I T <<<<<<<`);
+    WebhookProcessing(req, res);
+});
+
+
+app.listen(port, () => console.log(`qos-bot-api app listening on port ${port}!`))
+
