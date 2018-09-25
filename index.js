8cb5e16361de5499a9b98b7061a563b7afb1d4a7
Arun Narasani <narasani@versa-networks.com>
2018-09-24 17:20:58 -0700

    initial changes

--- index.js
+++ index.js
@@ -2,9 +2,35 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+var app = express()
+
+app.get('/hello', function (req, res) {
+  res.send('hello world')
+})
+
+app.get('/live/:device/:command', function (req, res) {
+  res.send(req.params.command + ' for ' + req.params.device + ' is ' + Math.random())
+})
+
+app.get('/trivia', function (req, res) {
+	var request = new XMLHttpRequest();
+	request.open('GET', 'http://numbersapi.com/random/trivia', false);  // `false` makes the request synchronous
+	request.send(null);
+
+	if (request.status === 200) {
+	  	return request.responseText;
+	} else {
+		return "Sorry, unable to play trivia right now!";
+	}
+
+  res.send('5600 is the number of metres above sea level of the highest bridge in the world, located in the Himalayan mountains.')
+})
+
+
+
+// express()
+//   .use(express.static(path.join(__dirname, 'public')))
+//   .set('views', path.join(__dirname, 'views'))
+//   .set('view engine', 'ejs')
+//   .get('/', (req, res) => res.render('pages/index'))
+//   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
