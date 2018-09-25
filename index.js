594fa3c2f0cecffa4479685c2b83e45cbcb16477
Arun Narasani <narasani@versa-networks.com>
2018-09-24 17:52:33 -0700

    updates

--- index.js
+++ index.js
@@ -1,34 +1,21 @@
-const express = require('express')
-const path = require('path')
-const port = process.env.PORT || 5000
+const express = require('express');
+const path = require('path');
+const port = process.env.PORT || 5000;
 
-var app = express()
+var app = express();
 
 app.get('/', function (req, res) {
-  res.send('hello world')
+  res.send('hello world');
 })
 
 app.get('/live/:device/:command', function (req, res) {
-  res.send(req.params.command + ' for ' + req.params.device + ' is ' + Math.random())
+  res.send(req.params.command + ' for ' + req.params.device + ' is ' + Math.random());
 })
 
 app.get('/trivia', function (req, res) {
-	// var request = new XMLHttpRequest();
-	// request.open('GET', 'http://numbersapi.com/random/trivia', false);  // `false` makes the request synchronous
-	// request.send(null);
-
-	// if (request.status === 200) {
-	//   	return request.responseText;
-	// } else {
-	// 	return "Sorry, unable to play trivia right now!";
-	// }
-
-  	return new (require('httpclient').HttpClient)({
-	    method: 'GET',
-	      url: 'http://numbersapi.com/random/trivia'
-	    }).finish().body.read().decodeToString();
-
-	// res.send('5600 is the number of metres above sea level of the highest bridge in the world, located in the Himalayan mountains.')
+	var request = require('sync-request');
+	var trivia = request('GET', 'http://numbersapi.com/random/trivia');
+	res.send(trivia)
 })
 
 app.listen(port, () => console.log(`qos-bot-api app listening on port ${port}!`))
